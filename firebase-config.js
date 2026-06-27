abf8ec6144199e2bb3d9aa85d366648a8ed3b985
Ajay <arya.aisakhi@gmail.com>
2026-06-27 23:58:12 +0530

    Deploy active Firebase config to git for shared member access

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-window.firebaseConfig = {};
+window.firebaseConfig = {
+    "apiKey": "AIzaSyBv04P_5SfjE1_2cX2C2KU-a5Wdse2CIyM",
+    "authDomain": "wealthplus-74f76.firebaseapp.com",
+    "projectId": "wealthplus-74f76",
+    "storageBucket": "wealthplus-74f76.firebasestorage.app",
+    "messagingSenderId": "86619786733",
+    "appId": "1:86619786733:web:7db5e85235c445855e6c4"
+};
